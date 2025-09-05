8af811a9b4e6682173a9aeda74182daddb78d36e
PioGodwin-M <piogodwin7@gmail.com>
2025-09-05 21:28:12 +0530

    fix : changes in symbols

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -154,7 +154,7 @@ const AllRooms = () => {
                 ))}
               </div>
 
-              <p className='text-xl font-medium text-gray-700'>${room.pricePerNight} /night</p>
+              <p className='text-xl font-medium text-gray-700'>₹ {room.pricePerNight} /night</p>
             </div>
           </div>
         ))}
